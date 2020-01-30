a20ded1720eddf5c74e1497852f18e44b7927479
xcixor <pndungu54@gmail.com>
2020-01-30 10:28:03 +0300

    chore(tests): add class setup

--- app/src/androidTest/java/com/example/flat/ApiUtilTest.java
+++ app/src/androidTest/java/com/example/flat/ApiUtilTest.java
@@ -1,6 +1,7 @@
 package com.example.flat;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.net.URL;
@@ -9,24 +10,31 @@ import static org.junit.Assert.*;
 
 public class ApiUtilTest {
     
-    private URL mExpectedBaseURL;
+    static URL sExpectedBaseURL;
+    static String BaseEndPoint = "/";
+    static ApiQueryBuilder sQueryBuilder;
 
+//    run before all tests
+    @BeforeClass
+    public static void classSetup(){
+        sQueryBuilder = new ApiQueryBuilder.QueryBuilder(BaseEndPoint).build();
+    }
+
+//    run before each test
     @Before
     public void setUp(){
-        String endPoint = "/";
-        ApiQueryBuilder queryBuilder = new ApiQueryBuilder.QueryBuilder(endPoint).build();
-        mExpectedBaseURL = queryBuilder.buildUrl();
+        sExpectedBaseURL = sQueryBuilder.buildUrl();
     }
 
     @Test
     public void buildUrl() {
-        String expectedStringUrl = this.mExpectedBaseURL.toString();
+        String expectedStringUrl = sExpectedBaseURL.toString();
         String fullUrl = "http://35.208.96.236/rooms/";
         assertEquals(expectedStringUrl, fullUrl);
     }
 
     @Test
     public void assertBuiltUrlIsURL(){
-        assertTrue(this.mExpectedBaseURL instanceof URL);
+        assertTrue(sExpectedBaseURL instanceof URL);
     }
 }
